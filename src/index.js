28fa29b3048d4911cb25dc627f0d5e28ed92640c
Andrew McIndoe <amcindoe@powerhrg.com>
2021-05-28 14:19:59 -0400

    Pair DOM lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ 
+  let newTask = document.querySelector("#new-task-description")
+  let form = document.querySelector("#create-task-form")
+
+  const addNewItem = (event) => {
+    event.preventDefault()
+    let element = document.createElement("li")
+    let list = document.querySelector("#tasks")
+    list.append(element)
+    element.innerText = newTask.value
+  }
+    form.addEventListener("submit", addNewItem)
+
 });
+    
